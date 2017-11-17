6e6f78492b3266a43573dafdc7d525f72ab71d70
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-11-17 12:05:37 +0100

    ajout sql partie1

---
+++ sql1.sql
@@ -0,0 +1,28 @@
+Exercice 1
+
+Créer une base de données languages.
+Commande en ligne de commande linux : CREATE DATABASE languages;
+Exercice 2
+
+Créer une base de données webDevelopment avec l’encodage UTF-8.
+Commande en ligne de commande linux : CREATE DATABASE webDevelopment CHARACTER SET 'utf8';
+Exercice 3
+
+Créer une base de données frameworks avec l’encodage UTF-8 si elle n’existe pas.
+Commande en ligne de commande linux : CREATE DATABASE frameworks CHARACTER SET 'utf8';
+Exercice 4
+
+Créer une base de données languages avec l’encodage UTF-8 si elle n’existe pas.
+Commande en ligne de commande linux : CREATE DATABASE languages CHARACTER SET 'utf8';
+Exercice 5
+
+Supprimer la base de données languages.
+Commande en ligne de commande linux : DROP DATABASE languages;
+Exercice 6
+
+Supprimer la base de données frameworks si elle existe.
+Commande en ligne de commande linux : DROP DATABASE frameworks;
+Exercice 7
+
+Supprimer la base de données languages si elle existe.
+Commande en ligne de commande linux : DROP DATABASE languages;
